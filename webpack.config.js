38a51e4e9cbc26171301f425172489f7794a99b4
Mathieu Santostefano <msantostefano@jolicode.com>
2018-06-17 19:13:47 +0200

    Fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const Encore = require('@symfony/webpack-encore');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 Encore
     .setOutputPath('build')
-    .setPublicPath('/build')
+    .setPublicPath(Encore.isProduction() ? '/oeil-du-cyclone/build' : '/build')
+    .setManifestKeyPrefix('oeil-du-cyclone')
     .cleanupOutputBeforeBuild()
     
     .addStyleEntry('css/style', './assets/scss/style.scss')
@@ -14,6 +16,10 @@ Encore
     .configureBabel(babelConfig => {
         babelConfig.presets.push('env');
     })
+
+    .addPlugin(new CopyWebpackPlugin([
+        { from: './assets/fonts', to: 'fonts/' },
+      ]))
 
     .enableSourceMaps(!Encore.isProduction())
     .enableBuildNotifications(!Encore.isProduction())
